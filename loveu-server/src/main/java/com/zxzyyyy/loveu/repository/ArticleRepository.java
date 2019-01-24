5c75b137193ae145ac6b06b6ba3208a5d57f0bff
fengyinglouzz <623385757@qq.com>
2019-01-25 00:01:10 +0800

    加es

--- loveu-server/src/main/java/com/zxzyyyy/loveu/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zxzyyyy.loveu.repository;
-
-import com.zxzyyyy.loveu.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Integer> {
-
-}
---
+++ loveu-server/src/main/java/com/zxzyyyy/loveu/repository/EsBlogRepository.java
@@ -0,0 +1,22 @@
+package com.zxzyyyy.loveu.repository;
+
+import com.zxzyyyy.loveu.entity.EsBlog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
+
+/**
+ * @Description:
+ */
+public interface EsBlogRepository extends ElasticsearchCrudRepository<EsBlog, String> {
+
+    /**
+     * 分页查询博客 （去重）
+     * @param title
+     * @param summary
+     * @param content
+     * @param pageable
+     * @return
+     */
+    Page<EsBlog> findDistinctByTitleContainingOrSummaryContainingOrContentContaining(String title, String summary, String content, Pageable pageable);
+}
